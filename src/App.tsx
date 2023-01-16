819b424fcbb363bfca6bfee10d5b4f5fbe63ded3
milyukov <milyukov@protei-lab.ru>
2023-01-16 09:24:47 +0300

    bootstrap practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import {BootstrapComponent} from "./bootstrap/BootstrapComponent";
 import {Practice} from "./bootstrap/Practice";
-import {JSPractice} from "./JSPractice/JSPractice";
 
 function App() {
   return (
     <div className="App h-100">
       <Practice/>
-      {/*<BootstrapComponent/>*/}
-      {/*<JSPractice/>*/}
     </div>
   );
 }
